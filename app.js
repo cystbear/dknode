f3bce6d2807d00cb6330fc74377dfb6770f726a5
Oleg Zinchenko <olegz@default-value.com>
2011-07-15 12:51:36 +0300

    added very simply mongoose crud

--- app.js
+++ app.js
@@ -37,5 +37,9 @@ app.get('/', function(req, res){
   });
 });
 
+// Model
+var model = require('./model');
+model.simpleCrud();
+
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
